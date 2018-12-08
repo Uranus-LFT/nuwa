d931cb472406a3e4d75930fe74abbc71c456d67a
alihanniba <alihanniba@gmail.com>
2018-12-08 17:12:39 +0800

    构造分表 sql 语句

--- venus/make.py
+++ venus/make.py
@@ -6,7 +6,7 @@ f = open("/Users/Alihanniba/Documents/reset.txt", encoding='utf-16')
 line = f.readline()
 while line:
     index = int(line) % 101
-    print("update comminfo.userprofile_%d set face_status = 0 where uid = %s limit 1;" % (
+    print("update alihaniba.louie_%d set face_status = 0 where uid = %s limit 1;" % (
         index, line))
     line = f.readline()
 f.close()
